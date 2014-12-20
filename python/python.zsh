1ee1ce734945a72e749e6ba636d61d368f79c7e3
Zack Lalanne <zack.lalanne@gmail.com>
2014-12-20 16:49:59 -0600

    Adding python tools

---
+++ python/python.zsh
@@ -0,0 +1,4 @@
+
+export WORKON_HOME=$HOME/.virtualenvs
+mkdir -p $WORKON_HOME
+source /usr/local/bin/virtualenvwrapper.sh
--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -42,7 +42,7 @@ COMPLETION_WAITING_DOTS="true"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(colored-man colorize cp extract history last-working-dir rsync vi-mode git python tmux)
+plugins=(colored-man colorize cp extract history last-working-dir rsync vi-mode git python tmux virtualenvwrapper)
 
 ZSH_TMUX_AUTOSTART=true
 
