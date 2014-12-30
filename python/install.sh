b8f1c46173e9a80fdc3a0849e5314b9df3b01c16
Zack Lalanne <zack.lalanne@ti.com>
2014-12-30 09:05:32 -0600

    Better install script

--- python/install.sh
+++ python/install.sh
@@ -2,6 +2,17 @@
 #
 # Install python packages
 
-echo "Installing python virtualenv tools"
-sudo pip install virtualenv
-sudo pip install virtualenvwrapper
+source ../script/print.sh
+
+user 'Install python virtualenv tools?'
+read -e status
+if [[ $status == "Y" || $status == "y" ]]; then
+    sudo pip install virtualenv
+    sudo pip install virtualenvwrapper
+fi
+
+user 'Install better bpython terminal?'
+read -e status
+if [[ $status == "Y" || $status == "y" ]]; then
+    sudo pip install bpython
+fi
