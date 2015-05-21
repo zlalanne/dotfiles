846258f67363cbd0b5813c0c8963b02b9956c408
Zack Lalanne <zack.lalanne@gmail.com>
2015-05-20 23:07:13 -0500

    Removing all install scripts and moving them to ansible playbook

--- vim/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Script to install Vundle so vim plugins will work
-
-if [[ -d ~/.vim/bundle ]]; then
-    echo "Vundle already installed"
-else
-    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
--- zsh/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Script to install oh-my-zsh so zsh plugins will work
-
-if [[ -d ~/.oh-my-zsh ]]; then
-    echo "Oh My Zsh already installed"
-else
-    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-fi
