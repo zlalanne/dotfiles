00f149cbc643115087efaec22f424b2add44b567
Zack Lalanne <zack.lalanne@gmail.com>
2015-07-06 20:18:10 -0500

    Adding elementary-os zsh plugin only for elementaryos

--- elementary/elementary.zsh
+++ elementary/elementary.zsh
@@ -1,1 +1,9 @@
-builtin . /usr/share/pantheon-terminal/enable-zsh-completion-notifications || builtin true
+if [ -f /etc/os-release ]
+then
+    name=$(cat /etc/os-release | grep "^NAME" | cut -d \" -f 2)
+    if [[ ${name} == *"elementary"* ]];
+    then
+        builtin . /usr/share/pantheon-terminal/enable-zsh-completion-notifications || builtin true
+    fi
+fi
+
