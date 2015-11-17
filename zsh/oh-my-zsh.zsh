98ac701ebea514321ff0226df745ce7541f2366f
Zack Lalanne <zack.lalanne@gmail.com>
2015-11-17 09:34:26 -0600

    Adding tmuxinator to base install

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -42,7 +42,7 @@ COMPLETION_WAITING_DOTS="true"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(colored-man colorize cp extract history ubuntu rsync git python jsontools vagrant tmux)
+plugins=(colored-man colorize cp extract history ubuntu rsync git python jsontools vagrant tmux tmuxinator)
 
 ZSH_TMUX_AUTOSTART=true
 
